252e2ea21841ad9196b7f3af88a7fa2175f76641
GustavoAlmeida-cy <gustavo.almeida.cy@gmail.com>
2025-07-04 00:21:01 -0300

    usercards

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,9 +1,21 @@
+import UserCard from "@/components/UserCard";
 import React from "react";
 
 const AdminPage = () => {
   return (
-    <div>
-      <h1>Admin</h1>
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* Left */}
+      <div className="w-full lg:w-2/3">
+        {/* User cards */}
+        <div className="flex gap-4 justify-between">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+      </div>
+      {/* Right */}
+      <div className="w-full lg:w-1/3">R</div>
     </div>
   );
 };

9ef84588d4fe7064c6ce71b16bc708780fd7a846
GustavoAlmeida-cy <gustavo.almeida.cy@gmail.com>
2025-07-04 16:48:47 -0300

    pagination & table-search

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,9 +1,33 @@
+import Pagination from "@/components/Pagination";
+import TableSearch from "@/components/TableSearch";
+import Image from "next/image";
 import React from "react";
 
 const TeacherListPage = () => {
   return (
-    <div>
-      <h1>Teachers</h1>
+    <div className="bg-white py-4 px-6 rounded-md flex-1 m-4 mt-0">
+      {/* Top */}
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4  w-full md:w-auto">
+          <TableSearch />
+          <div className="flex items-center gap-4 self-end mb-1">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/filter.png" alt="filter" width={15} height={15} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/sort.png" alt="filter" width={15} height={15} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/plus.png" alt="filter" width={15} height={15} />
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* List */}
+      <div></div>
+      {/* Pagination */}
+      <Pagination />
     </div>
   );
 };

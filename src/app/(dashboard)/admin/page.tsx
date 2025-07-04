bd0e74b4d25e5bcaa3a3d3243e247fb5cb7200ce
GustavoAlmeida-cy <gustavo.almeida.cy@gmail.com>
2025-07-04 00:48:51 -0300

    countchart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import CountChart from "@/components/CountChart";
 import UserCard from "@/components/UserCard";
 import React from "react";
 
@@ -13,6 +14,17 @@ const AdminPage = () => {
           <UserCard type="parent" />
           <UserCard type="staff" />
         </div>
+        {/* Middle charts */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* Count chart */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* Attendance chart */}
+          <div className="w-full lg:w-2/3 h-[450px]"></div>
+        </div>
+        {/* Bottom charts */}
+        <div></div>
       </div>
       {/* Right */}
       <div className="w-full lg:w-1/3">R</div>
